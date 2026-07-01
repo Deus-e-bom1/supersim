2f7988ee0cdf6c1824d84369f6a92bde69601645
Deus-e-bom1 <luongybb8d36@gmail.com>
2026-07-01 13:19:54 -0300

    Add files via upload

---
+++ test_api.php
@@ -0,0 +1,18 @@
+<?php
+$url = "https://api-apela.online/?user=a2a46935d4deb43874fcbf9828f00e10&cpf=00000000000";
+$context = stream_context_create([
+    "http" => [
+        "user_agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    ],
+    "ssl" => [
+        "verify_peer" => false,
+        "verify_peer_name" => false,
+    ]
+]);
+$res = @file_get_contents($url, false, $context);
+if ($res === false) {
+    echo "Error: ";
+    print_r(error_get_last());
+} else {
+    echo "Success: $res";
+}
